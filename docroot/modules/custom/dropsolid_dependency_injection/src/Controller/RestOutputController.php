cdd0ec2c3886768cee429049bbaa99ee159a31de
Juan M. Sosso <juan@servinube.net>
2019-02-10 22:58:46 +0100

    Use the new service for the page controller and block.

--- docroot/modules/custom/dropsolid_dependency_injection/src/Controller/RestOutputController.php
+++ docroot/modules/custom/dropsolid_dependency_injection/src/Controller/RestOutputController.php
@@ -5,43 +5,68 @@ namespace Drupal\dropsolid_dependency_injection\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\dropsolid_dependency_injection\RestConnectionInterface;
+use Drupal\dropsolid_dependency_injection\Services\PicturesServiceInterface;
 
 /**
- * Class RestOutputController
+ * Class RestOutputController.
+ *
  * @package Drupal\dropsolid_dependency_injection\Controller
  */
-class RestOutputController {
+class RestOutputController extends ControllerBase implements ContainerInjectionInterface {
+  /**
+   * The Album/Pictures service.
+   *
+   * @var Drupal\dropsolid_dependency_injection\Services\PicturesServiceInterface
+   */
+  private $pictureService;
 
   /**
+   * Create a new object.
+   *
+   * @param Drupal\dropsolid_dependency_injection\Services\PicturesServiceInterface $pictureService
+   *   Service to retrieve the remote pictures.
+   */
+  public function __construct(PicturesServiceInterface $pictureService) {
+    $this->pictureService = $pictureService;
+  }
+
+  /**
+   * Container injection implementation.
+   *
+   * @param Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The container.
+   */
+  public static function create(ContainerInterface $container): RestOutputController {
+    return new static(
+      $container->get('dropsolid_dependency_injection.pictures')
+    );
+  }
+
+  /**
+   * Return the renderable array with the pictures.
+   *
    * @return array
+   *   The array
    */
   public function showPhotos() {
     $build = [
       '#cache' => [
         'max-age' => 60,
-        'contexts' => ['url']
-      ]
+        'contexts' => ['url'],
+      ],
     ];
 
-    try {
-      $response = \Drupal::httpClient()->request('GET', "https://jsonplaceholder.typicode.com/albums/5/photos");
-      $data = $response->getBody()->getContents();
-      $decoded = json_decode($data);
-      if (!$decoded) {
-        throw new \Exception('Invalid data returned from API');
-      }
-    } catch (\Exception $e) {
-      return $build;
-    }
+    $decoded = $this->pictureService->getAlbum(5);
 
-    foreach ($decoded as $item) {
-      $build['rest_output_block']['photos'][] = [
-        '#theme' => 'image',
-        '#uri' => $item->thumbnailUrl,
-        '#alt' => $item->title,
-        '#title' => $item->title
-      ];
+    if (empty($decoded['error'])) {
+      foreach ($decoded as $item) {
+        $build['rest_output_block']['photos'][] = [
+          '#theme' => 'image',
+          '#uri' => $item->thumbnailUrl,
+          '#alt' => $item->title,
+          '#title' => $item->title,
+        ];
+      }
     }
 
     return $build;
--- docroot/modules/custom/dropsolid_dependency_injection/src/Plugin/Block/RestOutputBlock.php
+++ docroot/modules/custom/dropsolid_dependency_injection/src/Plugin/Block/RestOutputBlock.php
@@ -5,7 +5,7 @@ namespace Drupal\dropsolid_dependency_injection\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\dropsolid_dependency_injection\RestConnectionInterface;
+use Drupal\dropsolid_dependency_injection\Services\PicturesServiceInterface;
 
 /**
  * Provides a 'RestOutputBlock' block.
@@ -15,7 +15,32 @@ use Drupal\dropsolid_dependency_injection\RestConnectionInterface;
  *  admin_label = @Translation("Rest output block"),
  * )
  */
-class RestOutputBlock extends BlockBase {
+class RestOutputBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * The Album/Pictures service.
+   *
+   * @var Drupal\dropsolid_dependency_injection\Services\PicturesServiceInterface
+   */
+  private $pictureService;
+
+  /**
+   * Create a new object.
+   *
+   * @param Drupal\dropsolid_dependency_injection\Services\PicturesServiceInterface $pictureService
+   *   Service to retrieve the remote pictures.
+   */
+  public function __construct(PicturesServiceInterface $pictureService) {
+    $this->pictureService = $pictureService;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): RestOutputBlock {
+    return new static(
+      $container->get('dropsolid_dependency_injection.pictures')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -24,29 +49,23 @@ class RestOutputBlock extends BlockBase {
     $build = [
       '#cache' => [
         'max-age' => 60,
-        'contexts' => ['url']
-      ]
+        'contexts' => ['url'],
+      ],
     ];
 
-    try {
-      $albumId = random_int(1, 20);
-      $response = \Drupal::httpClient()->request('GET', "https://jsonplaceholder.typicode.com/albums/$albumId/photos");
-      $data = $response->getBody()->getContents();
-      $decoded = json_decode($data);
-      if (!$decoded) {
-        throw new \Exception('Invalid data returned from API');
-      }
-    } catch (\Exception $e) {
-      return $build;
-    }
+    $albumId = random_int(1, 20);
 
-    foreach ($decoded as $item) {
-      $build['rest_output_block']['photos'][] = [
-        '#theme' => 'image',
-        '#uri' => $item->thumbnailUrl,
-        '#alt' => $item->title,
-        '#title' => $item->title
-      ];
+    $decoded = $this->pictureService->getAlbum($albumId);
+
+    if (empty($decoded['error'])) {
+      foreach ($decoded as $item) {
+        $build['rest_output_block']['photos'][] = [
+          '#theme' => 'image',
+          '#uri' => $item->thumbnailUrl,
+          '#alt' => $item->title,
+          '#title' => $item->title,
+        ];
+      }
     }
 
     return $build;
